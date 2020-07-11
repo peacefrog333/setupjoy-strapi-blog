3ad70380b5057f85e257e40318cf47e49fda7a7b
Ali Azwad <eday369@gamail.com>
2020-07-11 11:50:30 +0800

    create table structures, environment files for dev and production

---
+++ config/env/development/database.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'database'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', 'root'),
+      },
+      options: {},
+    },
+  },
+});
