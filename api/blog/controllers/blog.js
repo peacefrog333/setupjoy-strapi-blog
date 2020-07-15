8034bbb6fd6e632b650c387149995d7d0e9b42ff
Ali Azwad <ali.azwad@nspa.gov.mv>
2020-07-15 22:00:02 +0800

    filter published blogs from api

--- api/blog/controllers/blog.js
+++ api/blog/controllers/blog.js
@@ -1,8 +1,26 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async find(ctx) {
+        let entities;
+    
+        ctx.query = {
+          ...ctx.query,
+          status: 'published',
+        };
+    
+        if (ctx.query._q) {
+          entities = await strapi.services.blog.search(ctx.query);
+        } else {
+          entities = await strapi.services.blog.find(ctx.query);
+        }
+    
+        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.blog }));
+      },
+};
